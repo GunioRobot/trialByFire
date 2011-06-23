11b4a401be5ff92caaec6b5e7fc6f932ee49d7d6
Caleb Gutshall <cgutshal@gmail.com>
2011-06-23 15:42:55 -0400

    working on while loop to print contents of ldap object

--- new-dns.py
+++ new-dns.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     # server = raw_input("Enter the ldapserver to connect to: ")
     # query = raw_input("Enter the inforamtion to be queryed: ") 
-
+#dn = raw_input("Enter the domain for the query")
 
     server = "ldap://masterldap.csee.wvu.edu"
     dn = "svn004"
@@ -15,11 +15,20 @@ if __name__ == "__main__":
     
 #connect to ldap
     l = ldap.initialize(server)
-    l.search(query)
+    
     
 #while stuff in object 
-    print host
-    print ipHostNumber
-    print macAddress
-    
-    
+    try:
+        result = l.search(query)    
+
+        while 1:
+            
+            if (data == []):
+                break
+            else:
+                print host
+                print ipHostNumber
+                print macAddress
+    except ldap.LDAPError, e:
+        print "ERROR connectiong to Ldap"
+        
